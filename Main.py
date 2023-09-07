d9a2beca1327e81b7bda6a7d55cb2aadf0f5d8a7
tu2-atmanand <sanketgauns8@gmail.com>
2023-09-07 15:40:01 +0530

    optimizing

--- Main.py
+++ Main.py
@@ -52,20 +52,6 @@ while True:
     # Perform inference on the frame
     res = seg_model(frame)
 
-    # result_pil_image = Image.fromarray(res[0].plot()[:, :, ::-1])
-
-    # # Convert the PIL image to an array to use with OpenCV
-    # result_cv_image = np.array(result_pil_image)
-
-    # # Display the result in the OpenCV window
-    # cv2.imshow("Result Image", result_cv_image)
-
-    # # Wait for 1 second (1000 milliseconds) before displaying the next frame
-    # cv2.waitKey(500)
-
-        # --------- list that stores the centroids of the current frame---------#
-    centr_pt_cur_fr = []
-
     # results = seg_model(frame)
     result = res[0]
     # ------- to get the classes of the yolo model to filter out the people---------------#
@@ -88,42 +74,6 @@ while True:
 
     # print("these are indexes:", idx)
 
-    # ----------- bounding boxes for person detections---------------#
-    bbox = []
-    for i in idx:
-        temp = bboxes[i]
-        # print("this is temp", temp)
-        bbox.append(temp)
-
-        # Convert to bbox to multidimensional list
-        box_multi_list = [arr.tolist() for arr in bbox]
-        # print("this are final human detected boxes")
-        # print(box_multi_list)
-
-    # ------------ drawing of bounding boxes-------------#
-    for box in box_multi_list:
-        (x, y, x2, y2) = box
-
-        cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 255), 2)
-        cx = int((x+x2)/2)
-        cy = int((y+y2)/2)
-        centr_pt_cur_fr.append((cx, cy))
-        cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-
-    # print("this are the centroids in the current frame")
-    # print(centr_pt_cur_fr)
-
-    # ------------- counting of total people in the footage ------------#
-    head_count = len(centr_pt_cur_fr)
-
-    # counting the number of faces with count_var variable
-    count_var = head_count
-
-    # displaying the face count on the screen for experiment purpose
-    # cv2.putText(frame, f'{head_count}', (10, 30),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
-    text = f'Head Count: {head_count}'
-    cv2.putText(frame, text, (15, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3, cv2.LINE_AA)
 
     cv2.imshow("Result Image", frame)
     # Check if the user presses the 'q' key to exit the loop
